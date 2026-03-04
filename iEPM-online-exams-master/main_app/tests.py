0090b1a41e661fae5adbf45fd3fc7518e66aca7d
Kirti1-Jha <Kirtijha2002@gmail.com>
2026-03-04 21:13:40 +0530

    Update tests.py

--- iEPM-online-exams-master/main_app/tests.py
+++ iEPM-online-exams-master/main_app/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 
+
+class SeleniumHomePageTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+
+    def test_homepage_load(self):
+        self.driver.get(self.live_server_url)
+
+        body = self.driver.find_element(By.TAG_NAME, "body")
+
+        self.assertIn("Online Exams", body.text)
+
+        time.sleep(3)
+
+    def tearDown(self):
+        self.driver.quit()
 # Create your tests here.
