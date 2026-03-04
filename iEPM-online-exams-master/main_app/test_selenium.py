d216631674d02dafedd4724052b3e64631777140
Kirti1-Jha <Kirtijha2002@gmail.com>
2026-03-04 22:24:16 +0530

    Update test_selenium.py

--- iEPM-online-exams-master/main_app/test_selenium.py
+++ iEPM-online-exams-master/main_app/test_selenium.py
@@ -1,22 +1,27 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from django.test import LiveServerTestCase
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-import time
 
 
 class SeleniumHomeTest(LiveServerTestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")   # required for Jenkins
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+
+        service = Service(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=service, options=chrome_options)
 
     def test_homepage_load(self):
         self.driver.get(self.live_server_url)
 
         body = self.driver.find_element(By.TAG_NAME, "body")
-        self.assertIn("Online Exams System", body.text)
-
-        time.sleep(2)
+        self.assertIn("Online Exams", body.text)
 
     def tearDown(self):
         self.driver.quit()
